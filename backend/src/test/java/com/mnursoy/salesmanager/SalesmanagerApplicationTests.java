3481629f3cacf0ea91ea3011f07ea3caf0fceeed
Muhammed Nursoy <muhammed.nursoy@inomera.com>
2019-03-08 18:54:44 +0300

    Bootstrap backend project

---
+++ backend/src/test/java/com/mnursoy/salesmanager/SalesmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mnursoy.salesmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
