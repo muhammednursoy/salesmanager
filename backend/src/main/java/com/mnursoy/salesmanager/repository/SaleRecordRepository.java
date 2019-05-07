9e45d0bdc47eaba5a179bb9505562656b8ad37e0
Muhammed Nursoy <muhammed.nursoy@inomera.com>
2019-05-07 13:21:37 +0300

    Fix amount conversion for gr/kg and lt/ml in sales report query

--- backend/src/main/java/com/mnursoy/salesmanager/repository/SaleRecordRepository.java
+++ backend/src/main/java/com/mnursoy/salesmanager/repository/SaleRecordRepository.java
@@ -25,7 +25,9 @@ public interface SaleRecordRepository extends JpaRepository<SaleRecord, Long> {
 		+ "group by month", nativeQuery = true)
 	List<MonthlyReport> getMonthlyIncome(@Param("productId") Long productId, @Param("fromDate") Date fromDate, @Param("toDate") Date toDate);
 
-	@Query(value = "select date_trunc('month', created_at) AS month, sum(sale_amount) as value "
+	@Query(value = "select "
+		+ "date_trunc('month', created_at) AS month, "
+		+ "sum(case when unit = 'GR' or unit = 'ML' then sale_amount / 1000 else sale_amount end) as value "
 		+ "from sale_record where "
 		+ "(sold_product_id = :productId) and "
 		+ "(date_trunc('month', created_at) >= cast(:fromDate as date)) and "
